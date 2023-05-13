2dbac4a52734eaef5a2054aba29ce3c9241ffde2
MD BIPLOB HOSSAIN <108410378+Biplob17h@users.noreply.github.com>
2023-05-13 02:51:47 -0600

    check 1

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4xdybXGxosMEdgM-n6eicBxM6_U2upJ8",
+  authDomain: "moviex1.firebaseapp.com",
+  projectId: "moviex1",
+  storageBucket: "moviex1.appspot.com",
+  messagingSenderId: "790823574881",
+  appId: "1:790823574881:web:ddc9f8673b000ac2a2b920"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
